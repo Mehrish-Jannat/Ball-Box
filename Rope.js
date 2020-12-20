559f6891d592b265f1340ad2c3b587cdc682d355
Mehrish-Jannat <72873955+Mehrish-Jannat@users.noreply.github.com>
2020-12-20 07:17:03 -0800

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,26 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1,
+            length: 300
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    display(){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+                strokeWeight(7);
+                line(pointA.x , pointA.y, pointB.x , pointB.y);
+           
+            
+            pop();
+    }
+    
+}
